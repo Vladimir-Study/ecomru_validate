6f7f28e8bcf4d1462e01891aded146598ae237c0
Vladimir Pavlov <volodia.pavloff201343@gmail.com>
2022-07-20 15:13:34 +0300

    Добавлены методы API POST, PUT, DELETE

--- API_account/API_account.py
+++ API_account/API_account.py
@@ -1,7 +1,8 @@
+import json
 from pprint import pprint
 
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 import psycopg2
 from validate_account.validate_account import read_account
 
@@ -21,10 +22,12 @@ def connections(auth_path: str):
 
 
 app = Flask(__name__)
-api = Api()
+api = Api(app)
+
+param_desk = {}
 
 
-class EcomruUsers(Resource):
+class UsersStatus(Resource):
 
     def get(self, id):
         try:
@@ -47,8 +50,103 @@ class EcomruUsers(Resource):
                 'Exception': e,
             }
 
-api.add_resource(EcomruUsers, '/account/status/<int:id>')
-api.init_app(app)
+
+class AddUser(Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("id", type=int, required=True)
+        parser.add_argument("mp_id", type=int, required=True)
+        parser.add_argument("client_secret_performance", type=str)
+        parser.add_argument("client_id", type=int, required=True)
+        parser.add_argument("client_id_performance", type=str)
+        parser.add_argument("client_id_api", type=str)
+        parser.add_argument("api_key", type=str)
+        parser.add_argument("campaigns_id", type=str)
+        parser.add_argument("name", type=str, required=True)
+        parser.add_argument("yandex_url", type=str)
+        parser.add_argument("internal_token", type=str)
+        add_params = parser.parse_args()
+        conn = connections('../login.txt')
+        with conn:
+            with conn.cursor() as cursor:
+                cursor.execute(f"SELECT * FROM account_list WHERE id = {add_params['id']}")
+                line_table = cursor.fetchone()
+                if line_table is not None:
+                    return {'Error': 'record with the same \'id\' exists'}
+                else:
+                    for key, val in add_params.items():
+                        if val is None:
+                            add_params[key] = 'null'
+                    cursor.execute(
+                        f"INSERT INTO account_list VALUES ({add_params['id']}, {add_params['mp_id']}, "
+                        f"'{add_params['client_secret_performance']}', {add_params['client_id']}, "
+                        f"'{add_params['client_id_performance']}', '{add_params['client_id_api']}', "
+                        f"'{add_params['api_key']}', '{add_params['campaigns_id']}', '{add_params['name']}', "
+                        f"'null', '{add_params['yandex_url']}', '{add_params['internal_token']}', 'null')"
+                    )
+                    conn.commit()
+        return add_params
+
+
+class EditUser(Resource):
+
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("id", type=int, required=True)
+        parser.add_argument("mp_id", type=int)
+        parser.add_argument("client_secret_performance", type=str)
+        parser.add_argument("client_id", type=int, required=True)
+        parser.add_argument("client_id_performance", type=str)
+        parser.add_argument("client_id_api", type=str)
+        parser.add_argument("api_key", type=str)
+        parser.add_argument("campaigns_id", type=str)
+        parser.add_argument("name", type=str)
+        parser.add_argument("yandex_url", type=str)
+        parser.add_argument("internal_token", type=str)
+        edit_params = parser.parse_args()
+        conn = connections('../login.txt')
+        with conn:
+            with conn.cursor() as cursor:
+                cursor.execute(f"SELECT * FROM account_list WHERE id = {edit_params['id']}")
+                line_table = cursor.fetchone()
+                if line_table is None:
+                    return {'Error': 'record with the same \'id\' missing'}
+                else:
+                    for key, val in edit_params.items():
+                        if val is None:
+                            edit_params[key] = 'null'
+                    cursor.execute(
+                        f"UPDATE account_list SET mp_id = {edit_params['mp_id']}, "
+                        f"client_secret_performance = '{edit_params['client_secret_performance']}', "
+                        f"client_id = {edit_params['client_id']}, client_id_performance = "
+                        f"'{edit_params['client_id_performance']}', client_id_api = '{edit_params['client_id_api']}', "
+                        f"api_key = '{edit_params['api_key']}', campaigns_id = '{edit_params['campaigns_id']}', "
+                        f"name = '{edit_params['name']}', yandex_url = '{edit_params['yandex_url']}', "
+                        f"internal_token = '{edit_params['internal_token']}' WHERE id = {edit_params['id']}"
+                    )
+                    conn.commit()
+        return edit_params
+
+
+class DeleteAccount(Resource):
+
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        delete_line = parser.parse_args()
+        conn = connections('../login.txt')
+        with conn:
+            with conn.cursor() as cursor:
+                cursor.execute(f"DELETE FROM account_list WHERE id = {delete_line['id']};")
+                conn.commit()
+        return {f"{delete_line['id']}": 'DELETE'}
+
+
+api.add_resource(UsersStatus, '/account/status/<int:id>')
+api.add_resource(AddUser, '/account/add')
+api.add_resource(EditUser, '/account/edit')
+api.add_resource(DeleteAccount, '/account/delete')
 
 
 if __name__ == '__main__':
--- API_account/test_request.py
+++ API_account/test_request.py
@@ -1,7 +1,11 @@
 from pprint import pprint
 import requests
+import json
 
-url = 'http://127.0.0.1:5000/account/status/255'
-response = requests.get(url)
+url = 'http://127.0.0.1:5000/account/delete'
+params = {
+    'Content-Type': 'application/json'
+}
+response = requests.delete(url, data=json.dumps({"id": 223}), headers=params)
 
-pprint(response.json())
+print(response.text)
